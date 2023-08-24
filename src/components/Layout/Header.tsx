531f18a3883fc7235291dd1448657bf85bf2315d
stuartErnstsen <stuarternstsen@gmail.com>
2023-08-23 19:52:57 -0600

    started fuck you bitch

--- src/components/Layout/Header.tsx
+++ src/components/Layout/Header.tsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import categories from '@/data/categories.json'
-type Props = {}
+import React from "react";
+import categories from "@/data/categories.json";
+type Props = {};
 
 export default function Header({}: Props) {
-    console.log('categories', categories)
+  console.log("categories", categories);
   return (
-    <div>Header</div>
-  )
+    <header className="py-6 px-4 border-b border-black">
+      <section className="flex items-center gap-2">
+        <div className="h-5 w-5 bg-black"></div>
+        <p>Coengine</p>
+      </section>
+    </header>
+  );
 }
