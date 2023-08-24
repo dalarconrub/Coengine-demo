9554a48a29efdd3eaeb38efcc10dc557c07f004c
BlairBryant <blairslc@gmail.com>
2023-08-23 20:18:56 -0700

    Boilerplate

--- src/app/products/[category]/page.tsx
+++ src/app/products/[category]/page.tsx
@@ -1,11 +1,18 @@
-import React from 'react'
+"use client"
+import React from "react"
+import products from "@/data/products.js"
 
 type Props = {
-    params: { category: string }
+  params: { category: string }
 }
 
-export default function ProductCategoryPage({ params: {category}}: Props) {
+export default function ProductCategoryPage({ params: { category } }: Props) {
+  console.log("PRODUCTS", products)
   return (
-    <div>Product Category {category}</div>
+    <div>
+      Product Category {category}
+      <img src={products[0].img} className="w-64 h-64" />
+      <img src={products[0].variants[0].img} className="w-24 h-24" />
+    </div>
   )
 }
