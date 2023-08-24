cec4094f604d513f7ad074d4aaf7f539c68f7142
BlairBryant <blairslc@gmail.com>
2023-08-23 18:41:15 -0700

    More boilerplate

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import categories from '@/data/categories.json'
+type Props = {}
+
+export default function Header({}: Props) {
+    console.log('categories', categories)
+  return (
+    <div>Header</div>
+  )
+}
